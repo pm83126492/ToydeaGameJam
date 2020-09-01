b753845c22bf80e29e1df3a3b7d668e325e4c6da
pm83126492 <pm83126492@gmail.com>
2020-09-02 00:08:50 +0800

    Add Circle
    Circle

---
+++ DogAndCat/Assets/Scripts/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public Transform B;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(B.position, Vector3.up, Speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
